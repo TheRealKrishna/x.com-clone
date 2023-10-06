06b5b2511160c2152963c986db86f197d9987635
TheRealKrishna <agarwl.krishna2@gmail.com>
2023-10-06 13:01:05 +0530

    updates

--- client/src/Pages/home/Components/Home.jsx
+++ client/src/Pages/home/Components/Home.jsx
@@ -1,8 +1,32 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import Styles from "../../../css/Home/Components/Home.module.css"
+import Globe from "../../../Images/Home/Globe.svg"
+import Gallery from "../../../Images/Home/Gallery.svg"
+import Gif from "../../../Images/Home/Gif.svg"
+import Poll from "../../../Images/Home/Poll.svg"
+import Emoji from "../../../Images/Home/Emoji.svg"
+import Schedule from "../../../Images/Home/Schedule.svg"
+import Location from "../../../Images/Home/Location.svg"
+
 
 export default function Home(props) {
     const [currentMenu, setCurrentMenu] = useState("For You")
+    const [post, setPost] = useState({ text: '', audience: "Everyone", whoCanReply: "Everyone can reply" });
+    const postTextInput = useRef();
+    const audienceSelector = useRef();
+    const whoCanReplySelector = useRef();
+
+    useEffect(() => {
+        postTextInput.current.addEventListener('focus', () => {
+            setTimeout(() => {
+                whoCanReplySelector.current.style.display = "block"
+            }, 300);
+            setTimeout(() => {
+                audienceSelector.current.style.display = "block"
+            }, 500)
+        })
+    }, [])
+
     return (
         <div className={Styles.container}>
             <div className={Styles.header}>
@@ -13,9 +37,25 @@ export default function Home(props) {
                 </div>
             </div>
             <div className={Styles.makePostContainer}>
-                <div className={Styles.inputContainer}>
-                    <div className={Styles.profileImageContainer}>
-                        <img src={props.user.profile} referrerPolicy="no-referrer" className={Styles.profileImage} alt="" />
+                <div className={Styles.profileImageContainer}>
+                    <img src={props.user.profile} referrerPolicy="no-referrer" className={Styles.profileImage} alt="" />
+                </div>
+                <div className={Styles.postBox}>
+                    <button ref={audienceSelector} style={{ display: "none" }} className={Styles.audienceSelectorButton}>{post.audience} <i class="fa-solid fa-angle-down"></i></button>
+                    <div className={Styles.postTextBox}>
+                        <input ref={postTextInput} type="text" value={post.text} className={Styles.postTextInput} placeholder='What is happening?!' />
+                    </div>
+                    <p ref={whoCanReplySelector} style={{ display: "none" }} className={Styles.whoCanReplySelector}><img className={Styles.whoCanReplySelectorGlobeIcon} src={Globe} alt='globe' />{post.whoCanReply}</p>
+                    <div className={Styles.postIconsAndSubmitContainer}>
+                        <div className={Styles.postIconsContainer}>
+                            <img src={Gallery} className={Styles.postIcon} alt="Gallery" />
+                            <img src={Gif} className={Styles.postIcon} alt="Gif" />
+                            <img src={Poll} className={Styles.postIcon} alt="Poll" />
+                            <img src={Emoji} className={Styles.postIcon} alt="Emoji" />
+                            <img src={Schedule} className={Styles.postIcon} alt="Schedule" />
+                            <img src={Location} className={`${Styles.postIcon} ${Styles.postIconDisabled}`} alt="Location" />
+                        </div>
+                        <button className={`${Styles.postButton} btn btn-primary rounded-pill`} disabled>Post</button>
                     </div>
                 </div>
             </div>
