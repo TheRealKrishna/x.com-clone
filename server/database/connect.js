f294a2d3750787205b2e6bae9dbdb6c5adf01f9e
TheRealKrishna <agarwl.krishna2@gmail.com>
2024-02-01 20:32:54 +0530

    updates

--- server/database/connect.js
+++ server/database/connect.js
@@ -1,12 +1,15 @@
 const Mongoose = require("mongoose")
 const dotenv = require('dotenv');
 dotenv.config({ path: './.env.local' });
+const errorHandler = require("../handler/errorHandler")
 
 function mongoConnect(){
     Mongoose.connect(process.env.MONGO_URI).then((result) => {
         console.log("MongoDB Connected to: x.com")  
     }).catch((err) => {
         console.log(err)
+        errorHandler(err)
+        mongoConnect()
     });
 }
 
